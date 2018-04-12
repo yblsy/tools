b8a708a295cd54e4fafba298bb06d0391180634d
liuchen <yebanliangshengy@163.com>
2018-04-12 17:15:28 +0800

    1

--- exceptiontool/src/main/java/personal/exception/LoginAppException.java
+++ exceptiontool/src/main/java/personal/exception/LoginAppException.java
@@ -1,11 +1,13 @@
 package personal.exception;
 
+import personal.enums.LoginAppEnum;
+
 /**
  * @author 刘晨
  * @create 2018-04-11 21:46
  * To change this template use File | Settings | Editor | File and Code Templates.
  **/
-public class LoginAppException extends Exception {
+public class LoginAppException extends RuntimeException {
 
     private static final long serialVersionUID = 8151091752866096602L;
 
@@ -23,6 +25,11 @@ public class LoginAppException extends Exception {
     public LoginAppException(String errorCode, String errorMsg, Throwable cause) {
         this(errorCode,errorMsg);
         this.cause = cause;
+    }
+
+    public LoginAppException(LoginAppEnum loginAppEnum){
+        this.errorCode = loginAppEnum.getErrorCode();
+        this.errorMsg = loginAppEnum.getErrorMsg();
     }
 
     public String getErrorCode() {
